730ae58bccf1cb9458543b276c7cf786fb12fe39
Hachiko264 <33091154+Hachiko264@users.noreply.github.com>
2020-04-21 00:43:42 +0200

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,65 +8,65 @@ return (
 		<>
 			<Row>
 				<Col md={6} className="detail-image">
-	            	<p><Image src={require('./images/ccc.jpg')} alt="Picture of Martha" fluid /></p>
+	            	<p><Image src={require('./images/ccc.jpg')} alt="webpage for CCC" fluid /></p>
 				</Col>
 
 				<Col md={6} className="detail_text">
-					<p><Image src={require('./images/happytails.jpg')} alt="Picture of Martha" fluid /></p>	
+					<p><Image src={require('./images/happytails.jpg')} alt="Animal App" fluid /></p>	
 				</Col>			
 			</Row>
 			<Row>
 				<Col md={8} className="detail-image">
-					<p><Image src={require('./images/kaffeserie.jpg')} alt="Picture of Martha" fluid /></p>				
+					<p><Image src={require('./images/kaffeserie.jpg')} alt="Bags of coffee" fluid /></p>				
 				</Col>
 
 				<Col md={4} className="detail_text">
-					<p><Image src={require('./images/poster.jpg')} alt="Picture of Martha" fluid /></p>				
+					<p><Image src={require('./images/poster.jpg')} alt="Graphic poster" fluid /></p>				
 				</Col>
 			</Row>
 			<Row>
 				<Col md={6} className="detail-image">
-					<p><Image src={require('./images/spacex.jpg')} alt="Picture of Martha" fluid /></p>				
+					<p><Image src={require('./images/spacex.jpg')} alt="Webpage about SpaceX" fluid /></p>				
 				</Col>
 
 				<Col md={6} className="detail_text">
-					<p><Image src={require('./images/bt.png')} alt="Picture of Martha" fluid /></p>				
+					<p><Image src={require('./images/bt.png')} alt="Newspaper" fluid /></p>				
 				</Col>
 			</Row>
 			<Row>
 				<Col md={4} className="detail-image">
-					<p><Image src={require('./images/solid.png')} alt="Picture of Martha" fluid /></p>				
+					<p><Image src={require('./images/solid.png')} alt="Social app" fluid /></p>				
 				</Col>
 
 				<Col md={8} className="detail_text">
-					<p><Image src={require('./images/murakami.png')} alt="Picture of Martha" fluid /></p>				
+					<p><Image src={require('./images/murakami.png')} alt="Murakami books" fluid /></p>				
 				</Col>
 			</Row>
 			<Row>
 				<Col md={6} className="detail-image">
-	            	<p><Image src={require('./images/alge.png')} alt="Picture of Martha" fluid /></p>
+	            	<p><Image src={require('./images/alge.png')} alt="Typescirpt design" fluid /></p>
 				</Col>
 
 				<Col md={6} className="detail_text">
-					<p><Image src={require('./images/dfirh.jpg')} alt="Picture of Martha" fluid /></p>	
+					<p><Image src={require('./images/dfirh.jpg')} alt="Webpage design" fluid /></p>	
 				</Col>			
 			</Row>
 			<Row>
 				<Col md={8} className="detail-image">
-					<p><Image src={require('./images/hig.png')} alt="Picture of Martha" fluid /></p>				
+					<p><Image src={require('./images/hig.png')} alt="Map app" fluid /></p>				
 				</Col>
 
 				<Col md={4} className="detail_text">
-					<p><Image src={require('./images/pechakucha.jpg')} alt="Picture of Martha" fluid /></p>				
+					<p><Image src={require('./images/pechakucha.jpg')} alt="Peacha Kucha poster" fluid /></p>				
 				</Col>
 			</Row>
 			<Row>
 				<Col md={6} className="detail-image">
-	            	<p><Image src={require('./images/ashes.jpg')} alt="Picture of Martha" fluid /></p>
+	            	<p><Image src={require('./images/ashes.jpg')} alt="Logo design" fluid /></p>
 				</Col>
 
 				<Col md={6} className="detail_text">
-					<p><Image src={require('./images/letha.jpg')} alt="Picture of Martha" fluid /></p>	
+					<p><Image src={require('./images/letha.jpg')} alt="Shoes website" fluid /></p>	
 				</Col>			
 			</Row>
         </>
